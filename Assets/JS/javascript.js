0454437bbc6f610cd8f46a6d12ec6b8770416931
Boivin <boivinhelene@yahoo.com>
2018-10-12 16:19:13 +0200

    JS Partie 3

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,15 @@
+// function colorGreen(){
+// document.getElementById("text").style.color = "green";
+// }
+// function colorRed(){
+// document.getElementById("text").style.color = "red";
+// }
+// function colorBlue(){
+// document.getElementById("text").style.color = "blue";
+// }
+
+// Avec 1 seule fonction, on ajoute un paramètre
+function changeColor(newColor){
+document.getElementById("text").style.color = "newColor";
+
+}
